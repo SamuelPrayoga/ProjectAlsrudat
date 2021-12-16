f87bc6aa6ea3ccda4eeb8f7f4e863505e5ccb6f7
Samuel Prayoga Tampubolon <80253461+SamuelPrayogaTampubolon@users.noreply.github.com>
2021-12-16 22:05:09 +0700

    Create driver

---
+++ driver.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+#include<windows.h>
+#include "login.c"
+
+int main(){	
+	printf("=========================================================\n");
+	printf("          Selamat Datang di Aplikasi Kereta Api          \n");
+	printf("=========================================================\n");
+	menu_utama();
+	return 0;
+}
